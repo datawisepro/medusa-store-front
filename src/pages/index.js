77605a3b71937e285537e27ac19c2fc56167aee5
Masho Ado <mashoado@gmail.com>
2023-02-27 23:23:40 +0300

    fix product name

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,23 +14,63 @@ const IndexPage = ({ product }) => {
   const router = useRouter()
 
   return (
-    <main>
-      <Head>
-        <title>Medusa</title>
-        <meta name="description" content="One-page checkout" />
-      </Head>
-      <Layout>
-        <Card sx={{ maxWidth: 256 }}>
-          <Flex>
-            <Image src={imageUrl} width="50" height="50" />
-            <Flex>
-              <Heading css={{ display: "blocks" }}>Product</Heading>
-              <Text css={{ display: "block" }}>Nice clean and Fresh</Text>
-            </Flex>
-          </Flex>
-        </Card>
-      </Layout>
-    </main>
+    <div className="products-page">
+      <h1>Products</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid">
+          {products.map(product => (
+            <Link href={`/products/${product.id}`} key={product.id}>
+              <div className="card">
+                <img src={product.thumbnail} alt={product.name} />
+                <h2>{product.title}</h2>
+                <p>{product.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+
+      <style jsx>{`
+        h1 {
+          color: orange;
+          text-align: center;
+          margin-bottom: 20px;
+        }
+        .grid {
+          display: grid;
+          grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+          gap: 2rem;
+        }
+
+        .card {
+          display: flex;
+          flex-direction: column;
+          background: white;
+          border: 1px solid #ddd;
+          border-radius: 4px;
+          padding: 1rem;
+          text-align: center;
+          transition: all 0.2s ease-in-out;
+        }
+
+        .card:hover {
+          transform: translateY(-5px);
+          box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+        }
+
+        .card img {
+          height: 200px;
+          object-fit: cover;
+          margin-bottom: 1rem;
+        }
+
+        .card h2 {
+          margin-bottom: 0.5rem;
+        }
+      `}</style>
+    </div>
   )
 }
 
